c52d7ea89e71b912fc1cd8469b737e8de6f94971
DivaAndini <143860881+DivaAndini@users.noreply.github.com>
2023-09-26 03:04:17 +0800

    Update and rename POSTEST_1.py to POSTEST 1.py

--- POSTEST 1.py
+++ POSTEST 1.py
@@ -4,11 +4,13 @@ MASUK
 ===== """)
 
 print()
+#memasukkan nama dan nim sebagai data
 print("Masukkan Nama dan NIM!")
 nama = input("Nama: ")
 nim = input("NIM: ")
 
 print()
+#memasukkan username dan password sesuai data
 print("Masuk ke Akun Menggunakan Nama dan NIM!")
 while True:
     a = input("Masukkan Username (Nama): ")
@@ -30,6 +32,7 @@ Menghitung Rumus Segitiga Pythagoras
 ====================================""")
 
 import math
+#fungsi untuk menentukan menu yang dipilih
 def menu_awal():
     sisi_yang_dihitung = int(input("""
 1. Alas
@@ -38,39 +41,47 @@ def menu_awal():
 4. Keluar
 Pilih sisi yang akan dihitung = """))
     if sisi_yang_dihitung == 1:
-        menghitung_alas()
+        menghitung_alas()                 #masuk ke fungsi menghitung alas
 
     elif sisi_yang_dihitung == 2:
-        menghitung_sisi_tegak()
+        menghitung_sisi_tegak()           #masuk ke fungsi menghitung sisi tegak
 
     elif sisi_yang_dihitung == 3:
-        menghitung_sisi_miring()
+        menghitung_sisi_miring()          #masuk ke fungsi menghitung sisi miring
 
     else: 
-        keluar()
+        keluar()                          #untuk keluar dari program
 
+#fungsi untuk menghitung alas
 def menghitung_alas():
     c = float(input("Masukkan panjang sisi miring = "))
     b = float(input("Masukkan panjang sisi tegak = "))
     a = float(math.sqrt(c**2 - b**2))
     print("Panjang alas segitiga adalah = ", a)
     menu_awal()
+    #kembali ke menu awal
 
+#fungsi untuk menghitung sisi tegak
 def menghitung_sisi_tegak():
     c = float(input("Masukkan panjang sisi miring = "))
     a = float(input("Masukkan panjang alas = "))
     b = float(math.sqrt(c**2 - a**2))
     print("Panjang sisi tegak segitiga = ", b)
     menu_awal()
+    #kembali ke menu awal
 
+#fungsi untuk menghitung sisi miring    
 def menghitung_sisi_miring():
     a = float(input("Masukkan panjang alas = "))
     b = float(input("Masukkan panjang sisi tegak = "))
     c = float(math.sqrt(a**2 + b**2))
     print("Panjang sisi miring segitiga =", c)
     menu_awal()
+    #kembali ke menu awal
 
+#fungsi untuk keluar program
 def keluar():
     print("Anda Keluar dari Akun !")
 
+#fungsi untuk memulai program
 menu_awal()
